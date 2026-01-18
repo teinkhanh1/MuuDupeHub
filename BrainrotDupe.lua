d508ee0d68f55f50ad955fe500bd7bcf8b8799e9
teinkhanh1 <dayneschannel4@gmail.com>
2026-01-18 22:42:03 +0000

    BrainrotDupe.lua

--- BrainrotDupe.lua
+++ BrainrotDupe.lua
@@ -1,6 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/teinkhanh1/MuuDupeHub/main/Brainrot.lua"))()
 
-game.Players.LocalPlayer.CharacterAdded:Connect(function()
-    wait(2)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/teinkhanh1/MuuDupeHub/main/Brainrot.lua"))()
-end)
